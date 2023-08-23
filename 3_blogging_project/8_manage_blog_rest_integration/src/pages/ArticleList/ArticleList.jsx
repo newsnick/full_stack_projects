5b2acbdcfbf9ac78c1df4cfb133e790a7334e4d5
newsnick <atlcontact8@gmail.com>
2023-08-23 10:10:47 +0200

    (8) use alternative font / background-color

--- 3_blogging_project/8_manage_blog_rest_integration/src/pages/ArticleList/ArticleList.jsx
+++ 3_blogging_project/8_manage_blog_rest_integration/src/pages/ArticleList/ArticleList.jsx
@@ -1,29 +1,8 @@
-// // ArticleList.jsx
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-// import './ArticleList.css'
+// ArticleList.jsx
 
-// export const ArticleList = ({ articles }) => {
-//   return (
-//     <>
-//       {articles.map((article) => (
-//         <Link
-//           key={article.articleId}
-//           className="articles-list-item"
-//           to={`/articles/${article.articleId}`}
-//         >
-//           <h2>{article.title}</h2>
-//           <p>{article.content[0].substring(0, 150)}...</p>
-//         </Link>
-//       ))}
-//     </>
-//   )
-// }
-
-// export default ArticleList
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './ArticleList.css' // Import the ArticleList.css
+import './ArticleList.css'
 
 export const ArticleList = ({ articles }) => {
   return (
--- 3_blogging_project/8_manage_blog_rest_integration/src/pages/ArticlePage/ArticlePage.jsx
+++ 3_blogging_project/8_manage_blog_rest_integration/src/pages/ArticlePage/ArticlePage.jsx
@@ -1,35 +1,9 @@
 // // ArticlePage.jsx
-// import React from 'react'
-// import { useParams } from 'react-router-dom'
-// import { NotFoundPage } from '../NotFoundPage/NotFoundPage'
-// import './ArticlePage.css'
 
-// export const ArticlePage = ({ articles }) => {
-//   const params = useParams()
-//   const articleId = params.articleId
-
-//   const article = articles.find((article) => article.articleId === articleId)
-
-//   if (!article) {
-//     return <NotFoundPage />
-//   }
-
-//   return (
-//     <>
-//       <h2>{article.title}</h2>
-//       <b className="greenbox">This article has {article.upvotes} upvotes</b>
-//       {article.content.map((paragraph, index) => (
-//         <p key={index}>{paragraph}</p>
-//       ))}
-//     </>
-//   )
-// }
-
-// export default ArticlePage
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { NotFoundPage } from '../NotFoundPage/NotFoundPage'
-import './ArticlePage.css' // Import the ArticlePage.css
+import './ArticlePage.css'
 
 export const ArticlePage = ({ articles }) => {
   const params = useParams()
